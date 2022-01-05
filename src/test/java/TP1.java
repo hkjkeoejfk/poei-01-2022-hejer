d22c5ae068d773251b70075eda19996350a7a05f
hejer <hejer.rjiba.75@gmail.com>
2022-01-05 11:11:26 +0100

    nouveau commit

--- src/test/java/TP1.java
+++ src/test/java/TP1.java
@@ -5,20 +5,27 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 
 public class TP1 {
 
+    WebDriver driver;
+
+    @BeforeMethod
+        public void setup() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.fr");
+        driver.manage().window().maximize();
+
+        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
+        buttonCookies.click();
+    }
    @Test
     public void test1 (){
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://www.amazon.fr");
-    driver.manage().window().maximize();
 
-    WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
-    buttonCookies.click();
     WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
     barreRecherche.sendKeys("machine à raclette");
     barreRecherche.sendKeys(Keys.ENTER);
@@ -33,12 +40,7 @@ public class TP1 {
     @Test
     public void test2 (){
 
-      WebDriver driver = new ChromeDriver();
-      driver.get("https://www.amazon.fr");
-      driver.manage().window().maximize();
 
-      WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
-      buttonCookies.click();
       WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
       barreRecherche.sendKeys("machine à raclette");
       barreRecherche.sendKeys(Keys.ENTER);
