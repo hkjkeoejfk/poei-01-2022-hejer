551dd32e0e6caedfc0068afff9497650e8d94f36
hejer <hejer.rjiba.75@gmail.com>
2022-01-05 15:11:44 +0100

    Livres

--- src/test/java/TP1.java
+++ src/test/java/TP1.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -63,5 +65,16 @@ public class TP1 {
      photo.click();
 
 
+    }
+    @Test
+    public void testExplicitWait() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        driver.findElement(By.id("nav-hamburger-menu")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".hmenu-item[data-menu-id='10']")));
+        driver.findElement(By.cssSelector(".hmenu-item[data-menu-id='10']")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.hmenu-visible > li:nth-child(3)")));
+        driver.findElement(By.cssSelector("ul.hmenu-visible > li:nth-child(3) > a")).click();
     }
 }
