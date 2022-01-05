8364a5dd003b40d31d484e14ace74fa5906316da
hejer <hejer.rjiba.75@gmail.com>
2022-01-05 12:24:29 +0100

    MAJ

--- src/test/java/TP1.java
+++ src/test/java/TP1.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 
 
 public class TP1 {
@@ -18,6 +19,7 @@ public class TP1 {
     @BeforeMethod
         public void setup() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.get("https://www.amazon.fr");
         driver.manage().window().maximize();
 
